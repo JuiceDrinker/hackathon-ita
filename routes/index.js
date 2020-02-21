3081335152316ae9e4f43baf253e4b6bd58f625e
JuiceDrinker <subramanianaditya42@gmail.com>
2020-02-21 13:20:53 +0100

    added axios, updating styles on index.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const router  = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-router.get('/sports', (req, res, next) => {
-  res.render('sports');
+router.get("/sports", (req, res, next) => {
+  res.render("sports");
 });
 
-router.get('/olympic', (req, res, next) => {
-  res.render('olympic');
+router.get("/olympic", (req, res, next) => {
+  res.render("olympic");
 });
-
 
 module.exports = router;
