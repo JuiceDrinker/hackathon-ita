b1d8df4abaa9f224c7ee8f76aac4ddcb23ac598f
JuiceDrinker <subramanianaditya42@gmail.com>
2020-02-21 14:19:35 +0100

    index.hbs view is now working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 const axios = require("axios");
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  axios
+    .get("https://jsondata-italy.herokuapp.com/medals_by_year")
+    .then(yearlData => {
+      const data = yearlData.data.sort((a, b) => {
+        return b.year - a.year;
+      });
+      res.render("index", { data });
+    })
+    .catch(err => {});
 });
 
 router.get("/sports", (req, res, next) => {
